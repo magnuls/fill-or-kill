b27bdaedbbb52eb6aa652edef3079e1dc994eaf2
magnus <magnuspicataggio@gmail.com>
2026-09-21 22:27:31 -0400

    Ignore scratch notes, DS_Store and generated feeds

--- include/lob/OrderBook.hpp
+++ include/lob/OrderBook.hpp
@@ -5,7 +5,8 @@
 #include "lob/Trade.hpp"
 #include "lob/Types.hpp"
 
-// Interface every book implementation exposes. Strategies program against this.
+// Interface every book implementation exposes
+// Strategies program against this
 class OrderBook {
   public:
     virtual ~OrderBook() = default;
@@ -13,9 +14,10 @@ class OrderBook {
     virtual Trades AddOrder(const Order& order) = 0;
     virtual void CancelOrder(Id orderId) = 0;
 
-    // Resting quantity at a price on the given side; 0 if the level is empty.
+    // Resting quantity at a price on the given side
+    // 0 if the level is empty
     virtual Quantity LevelTotal(Price p, bool bidSide) const = 0;
     virtual bool Contains(Id id) const = 0;
 };
 
-#endif // LOB_ORDERBOOK_HPP
+#endif
